d5c9e430e7639e2e598564f83b413be2579ce1e4
sanchez-herlinda <a.23308051280344@cbtis128.edu.mx>
2025-10-06 12:08:47 -0600

    first commit

---
+++ app_pedidos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Pedidos(models.Model):
+    total=models.DecimalField(max_digits=10, decimal_places=2)
+    estado=models.CharField(max_length=50, default='Pendiente')
+# Es para administrador
+    def __str__(self):
+        return f"{self.total} {self.estado}"
